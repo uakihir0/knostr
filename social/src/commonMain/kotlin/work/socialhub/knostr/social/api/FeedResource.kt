78cf3e5c2c1a540d584a12324dc19c5433f5a63e
Akihiro Urushihara <a.urusihara@gmail.com>
2026-05-08 21:54:14 +0900

    add excludeSensitive parameter to feed query methods

--- social/src/commonMain/kotlin/work/socialhub/knostr/social/api/FeedResource.kt
+++ social/src/commonMain/kotlin/work/socialhub/knostr/social/api/FeedResource.kt
@@ -10,16 +10,16 @@ import kotlin.js.JsExport
 interface FeedResource {
 
     /** Get home feed (posts from followed users) */
-    suspend fun getHomeFeed(since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    suspend fun getHomeFeed(since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     /** Get a single note by event ID */
     suspend fun getNote(eventId: String): Response<NostrNote>
 
     /** Get notes posted by a specific user */
-    suspend fun getUserFeed(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    suspend fun getUserFeed(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     /** Get notes mentioning the authenticated user (p-tag) */
-    suspend fun getMentions(since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    suspend fun getMentions(since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     /** Get thread context (ancestors + descendants) for a note */
     suspend fun getThread(eventId: String): Response<NostrThread>
@@ -59,25 +59,25 @@ interface FeedResource {
     suspend fun delete(eventId: String, reason: String = ""): Response<Boolean>
 
     /** Get posts that the user has liked (via kind:7 reaction lookup) */
-    suspend fun getUserLikesFeed(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    suspend fun getUserLikesFeed(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     /** Get posts with media attachments (via imeta tag filter) */
-    suspend fun getUserMediaFeed(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    suspend fun getUserMediaFeed(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     /** Resolve note1... bech32 to event */
     suspend fun getNoteByNpub(noteId: String): Response<NostrNote>
 
     @JsExport.Ignore
-    fun getHomeFeedBlocking(since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    fun getHomeFeedBlocking(since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     @JsExport.Ignore
     fun getNoteBlocking(eventId: String): Response<NostrNote>
 
     @JsExport.Ignore
-    fun getUserFeedBlocking(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    fun getUserFeedBlocking(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     @JsExport.Ignore
-    fun getMentionsBlocking(since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    fun getMentionsBlocking(since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     @JsExport.Ignore
     fun getThreadBlocking(eventId: String): Response<NostrThread>
@@ -117,10 +117,10 @@ interface FeedResource {
     fun deleteBlocking(eventId: String, reason: String = ""): Response<Boolean>
 
     @JsExport.Ignore
-    fun getUserLikesFeedBlocking(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    fun getUserLikesFeedBlocking(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     @JsExport.Ignore
-    fun getUserMediaFeedBlocking(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50): Response<List<NostrNote>>
+    fun getUserMediaFeedBlocking(pubkey: String, since: Long? = null, until: Long? = null, limit: Int = 50, excludeSensitive: Boolean = false): Response<List<NostrNote>>
 
     @JsExport.Ignore
     fun getNoteByNpubBlocking(noteId: String): Response<NostrNote>
