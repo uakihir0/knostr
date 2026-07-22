89cf7815c9c919c7b65f066dc647f1b776f72ac7
Akihiro Urushihara <a.urusihara@gmail.com>
2026-07-22 20:39:39 +0900

    feat: add requireCompleteData utility for response completeness validation

--- social/src/commonMain/kotlin/work/socialhub/knostr/social/internal/ResponseMapping.kt
+++ social/src/commonMain/kotlin/work/socialhub/knostr/social/internal/ResponseMapping.kt
@@ -1,5 +1,6 @@
 package work.socialhub.knostr.social.internal
 
+import work.socialhub.knostr.NostrException
 import work.socialhub.knostr.api.response.Response
 
 internal fun <T, R> Response<T>.withData(data: R): Response<R> {
@@ -7,6 +8,13 @@ internal fun <T, R> Response<T>.withData(data: R): Response<R> {
         it.json = json
         it.isComplete = isComplete
     }
+}
+
+internal fun <T> Response<T>.requireCompleteData(operation: String): T {
+    if (!isComplete) {
+        throw NostrException("Cannot $operation because the relay query was incomplete")
+    }
+    return data
 }
 
 internal fun <T> responseOf(data: T, vararg sources: Response<*>): Response<T> {
